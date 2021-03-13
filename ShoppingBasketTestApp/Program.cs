f991091a1284f74b48e4d08e320f2008fa54f474
Tomislav Kulusic <tomokulusic@gmail.com>
2021-03-13 15:43:05 +0100

    Project Setup: - Added README file - Added testing console app

---
+++ ShoppingBasketTestApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using ShoppingBasket;
+using System;
+
+namespace ShoppingBasketTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Shopping Basket App!");
+
+            var shoppingBasket = new ShoppingBasketClass();
+        }
+    }
+}
